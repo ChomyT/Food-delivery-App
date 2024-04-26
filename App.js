b22ff18afc675822be8a2e4322e6c5ef150c3981
Martina Ngwuta <martinangwuta@gmail.com>
2024-04-26 06:34:09 +0100

    message

---
+++ App.js
@@ -0,0 +1,20 @@
+/*var hamburger = document.getElementById('hamburger')
+const mobileMenu = document.getElementById('mobile')
+hamburger.addEventListener('click', () => {
+  if(mobileMenu.style.display == 'block'){
+    mobileMenu.style.display = 'none'
+  
+  }else {
+    mobileMenu.style.display = 'block'
+  }
+  
+})*/
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
